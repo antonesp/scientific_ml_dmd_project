10f46465033ff54e67b7ce5b9cb056809bc9bd9f
ClaraHol <s214722@dtu.dk>
2026-04-16 11:00:37 +0200

    More data stuff

--- src/mr_dmd/data.py
+++ src/mr_dmd/data.py
@@ -2,13 +2,14 @@ from pathlib import Path
 
 import typer
 import netCDF4
+import numpy as np
 from torch.utils.data import Dataset
 
 
 class ncDataset(Dataset):
     """My custom dataset."""
 
-    def __init__(self, data_path: Path) -> None:
+    def __init__(self, data_path: str) -> None:
         self.data_path = data_path
         self.data = netCDF4.Dataset(self.data_path)
 
@@ -18,15 +19,18 @@ class ncDataset(Dataset):
     def __getitem__(self, index: int):
         """Return a given sample from the dataset."""
 
-    def preprocess(self, output_folder: Path) -> None:
+    def preprocess(self, output_folder: str) -> None:
         """Preprocess the raw data and save it to the output folder."""
+        sst = self.data["ssf"][:, :, :]
+        np.save(output_folder, sst)
 
 
-def preprocess(data_path: Path, output_folder: Path) -> None:
+
+def preprocess(data_path: str, output_folder: str) -> None:
     print("Preprocessing data...")
-    dataset = MyDataset(data_path)
+    dataset = ncDataset(data_path)
     dataset.preprocess(output_folder)
 
 
 if __name__ == "__main__":
-    typer.run(preprocess("data/"))
+    typer.run(preprocess())
--- src/mr_dmd/visualize.py
+++ src/mr_dmd/visualize.py
@@ -4,4 +4,4 @@ import matplotlib.pyplot as plt
 fb = "data/sst.mon.ltm.1981-2010.nc"
 nc = netCDF4.Dataset(fb)
 
-print(nc)
+print(type(nc["sst"]))
