628638940f106816695c08808e88b4d904061a29
antonesp <antonespholm@gmail.com>
2026-04-30 11:29:16 +0200

    Added data processing and testing of mrDMD on the data

--- src/mr_dmd/data.py
+++ src/mr_dmd/data.py
@@ -21,7 +21,7 @@ class ncDataset(Dataset):
 
     def preprocess(self, output_folder: str) -> None:
         """Preprocess the raw data and save it to the output folder."""
-        sst = self.data["ssf"][:, :, :]
+        sst = self.data["sst"][:, :, :]
         np.save(output_folder, sst)
 
 
@@ -31,6 +31,27 @@ def preprocess(data_path: str, output_folder: str) -> None:
     dataset = ncDataset(data_path)
     dataset.preprocess(output_folder)
 
+def preprocess2(data_path: str, output_folder_data: str, output_folder_mask: str) -> None:
+    nc = netCDF4.Dataset(data_path)
+    sst = nc["sst"][:,:,:]
+
+    
+    sst_flat = sst.reshape(sst.shape[0], -1).T
+    
+    mask = sst_flat.mask[:,0]
+
+
+    sst_valid = sst_flat.compressed().reshape(-1, 2115)
+    # print(sst_valid.shape)
+
+    # print(type(mask))
+    # print(type(sst_valid))
+    np.save(output_folder_data, sst_valid)
+    np.save(output_folder_mask, mask)
+
 
 if __name__ == "__main__":
-    typer.run(preprocess())
+    input_folder = "data/sst.mon.mean.nc"
+    output_folder_data = "data/sst"
+    output_folder_mask = "data/sst_mask"
+    preprocess2(input_folder, output_folder_data, output_folder_mask)
--- src/mr_dmd/visualize.py
+++ src/mr_dmd/visualize.py
@@ -1,7 +1,26 @@
 import netCDF4
 import matplotlib.pyplot as plt
+import numpy as np
 
-fb = "data/sst.mon.ltm.1981-2010.nc"
+fb = "data/sst.mon.mean.nc"
 nc = netCDF4.Dataset(fb)
+sst = nc["sst"][:,:,:]
 
-print(type(nc["sst"]))
+# test = np.load("data/sst.npy")
+
+# print(type(test))
+
+# print(test.shape)
+print(type(sst))
+# flatten dimensions 1 and 2 (combine lat/lon) so result is (time, space)
+sst_flat = sst.reshape(sst.shape[0], -1)
+print(type(sst_flat))
+print(sst_flat.shape)
+
+print("mean at start")
+print(np.mean(sst[0,:,:]))
+
+print("mean at end")
+print(np.mean(sst[-1, :, :]))
+
+
